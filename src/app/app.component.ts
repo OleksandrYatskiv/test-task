4e5db92fd171440087659886b8399d85860168ab
Sashko <yatsk1660@gmail.com>
2023-11-10 22:56:59 +0100

    Password checker
    Password checker made with Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,72 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  styleUrls: ['./app.component.css'],
+  template: `
+    <div>
+      <h1>Password Strength Checker</h1>
+      <label>Password:</label>
+      <input type="password" (input)="checkPasswordStrength($event)" />
+      <div [style.color]="passwordStrengthColor()">
+        <div [style.background-color]="sectionColor(1)"></div>
+        <div [style.background-color]="sectionColor(2)"></div>
+        <div [style.background-color]="sectionColor(3)"></div>
+      </div>
+    </div>
+  `,
 })
 export class AppComponent {
-  title = 'test-task';
+  password: string = '';
+
+  checkPasswordStrength(event: Event): void {
+    const password = (event.target as HTMLInputElement).value;
+    this.password = password;
+  }
+
+  passwordStrengthColor(): string {
+    if (!this.password) {
+      return 'gray';
+    } else if (this.password.length < 8) {
+      return 'red';
+    } else if (this.isPasswordStrong()) {
+      return 'green';
+    } else if (this.isPasswordMedium()) {
+      return 'yellow';
+    } else {
+      return 'red';
+    }
+  }
+
+  sectionColor(sectionNumber: number): string {
+    const password = this.password;
+    if (!password) {
+      return 'gray';
+    } else if (password.length < 8) {
+      return 'red';
+    }
+
+    if (this.isPasswordStrong() && sectionNumber <= 3) {
+      return 'green';
+    } else if (this.isPasswordMedium() && sectionNumber <= 2) {
+      return 'yellow';
+    } else if (sectionNumber === 1) {
+      return 'red';
+    }
+
+    return 'gray';
+  }
+
+  private isPasswordStrong(): boolean {
+    return /[a-zA-Z]/.test(this.password) && /\d/.test(this.password) && /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(this.password);
+  }
+
+  private isPasswordMedium(): boolean {
+    return (
+      /[a-zA-Z]/.test(this.password) && (/\d/.test(this.password) || /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(this.password)) ||
+      /\d/.test(this.password) && /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(this.password)
+    );
+  }
 }
+
